6b38c6a0ae0e55016da33047a73d2f189f6d73ca
ahmadalrasheed <>
2021-06-03 11:14:29 +0000

    javascript

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var feedback =prompt("what do you think about my website ? answer with good or bad ?")
+
+if (feedback=="good"){
+
+  alert("thank you ! i appreciate your feedback!")
+}
+else if(feedback=="bad") {
+alert("i will try to make it good ! thank you for your feedback!")
+
+}
+else {
+  alert("please answer only with good or bad !")
+}
+
+confirm("Press a button!");
+
+document.write('<p style="color:green">This is From JavaScript !! welcome to my website !</p>')
