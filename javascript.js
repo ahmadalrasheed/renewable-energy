8d00e544f03bbc4cb9f6bcb96a7c3285fc82910b
ahmadalrasheed <>
2021-06-03 12:31:59 +0000

    javascript1

--- javascript.js
+++ javascript.js
@@ -15,3 +15,27 @@ else {
 confirm("Press a button!");
 
 document.write('<p style="color:green">This is From JavaScript !! welcome to my website !</p>')
+
+
+var username= prompt("what is your name?")
+
+document.write("<h2>Welcome "+ username +" to my website</h2>")
+
+var username= prompt("do you prefer using solar energy or electrical energy ?")
+
+if (username=="solar energy"){
+  document.write('<img src="https://tse3.mm.bing.net/th?id=OIP.9asGhLf4LLdXWtrgikSUtQHaEK&pid=Api&P=0&w=279&h=158" title="solar energy" />')
+  
+} else if(username=="electrical energy"){
+    document.write('<img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEPDGar37PY_RolPaLuTlic0wGBqTAqMLs-w&usqp=CAU  title="electrical energy" />')
+
+
+} else{
+  alert("please choose solar energy or electrical energy!!")
+}
+
+
+
+
+
+
